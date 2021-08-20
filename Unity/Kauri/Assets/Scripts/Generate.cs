3b107846c7925f0a6e20f8d734d44c952dc3b322
Hannah <hoganksteffes@gmail.com>
2021-08-19 23:23:43 -0500

    Add Unity Project

---
+++ Unity/Kauri/Assets/Scripts/Generate.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Generate : MonoBehaviour
+{
+    public Vector3 startingPoint;
+
+    class Branch
+    {
+        Vector3 start;
+        Vector3 end;
+        Branch parent;
+        List<Branch> children;
+        int distanceFromRoot;
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
