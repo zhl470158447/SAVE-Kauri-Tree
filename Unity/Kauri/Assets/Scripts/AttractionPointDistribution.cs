fef14152883eae0fa7da148f4a81a241d17abf85
Hannah <hoganksteffes@gmail.com>
2021-09-21 22:03:14 -0500

    Create class for generating attraction point distributions

---
+++ Unity/Kauri/Assets/Scripts/AttractionPointDistribution.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AttractionPointDistribution
+{
+    public List<Vector3> GenerateAttractorsSpherical(int n, float r, Vector3 start)
+    {
+        List<Vector3> points = new List<Vector3>();
+        for (int i = 0; i < n; i++)
+        {
+            float radius = Random.Range(0f, 1f);
+            radius = Mathf.Pow(Mathf.Sin(radius * Mathf.PI / 2f), 0.8f);
+            radius *= r;
+            // 2 angles are generated from which a direction will be computed
+            float alpha = Random.Range(0f, Mathf.PI);
+            float theta = Random.Range(0f, Mathf.PI * 2f);
+
+            Vector3 pt = new Vector3(
+                radius * Mathf.Cos(theta) * Mathf.Sin(alpha),
+                radius * Mathf.Sin(theta) * Mathf.Sin(alpha),
+                radius * Mathf.Cos(alpha)
+            );
+
+            // translation to match the parent position
+            pt += start - new Vector3(0, r, 0);
+
+            points.Add(pt);
+        }
+        return points;
+    }
+    public List<Vector3> GenerateAttractorsHemisphere(int n, float r, Vector3 start)
+    {
+        List<Vector3> points = new List<Vector3>();
+        for (int i = 0; i < n; i++)
+        {
+            float radius = Random.Range(0f, 1f);
+            radius = Mathf.Pow(Mathf.Sin(radius * Mathf.PI / 2f), 0.8f);
+            radius *= r;
+            // 2 angles are generated from which a direction will be computed
+            float alpha = Random.Range(0f, Mathf.PI);
+            float theta = Random.Range(0f, Mathf.PI);
+
+            Vector3 pt = new Vector3(
+                radius * Mathf.Cos(theta) * Mathf.Sin(alpha),
+                radius * Mathf.Sin(theta) * Mathf.Sin(alpha),
+                radius * Mathf.Cos(alpha)
+            );
+
+            // translation to match the parent position
+            pt += start + new Vector3(0, r, 0);
+
+            points.Add(pt);
+        }
+        return points;
+    }
+}
