0424808a5697e994e55751476a73ef0d8c5837cb
Hannah <hoganksteffes@gmail.com>
2021-09-30 21:36:48 -0500

    Start of updated dist for Mature branches

--- Unity/Kauri/Assets/Scripts/AttractionPointDistribution.cs
+++ Unity/Kauri/Assets/Scripts/AttractionPointDistribution.cs
@@ -55,6 +55,38 @@ public class AttractionPointDistribution
         return points;
     }
 
+    public List<Vector3> GenerateAttractorsMatureBranches(int n, float r, Vector3 start)
+    {
+        Vector3 center = start + new Vector3(0, r, 0);
+        List<Vector3> points = new List<Vector3>();
+        while(points.Count<n)
+        {
+            float radius = Random.Range(0f, 1f);
+            radius = Mathf.Pow(Mathf.Sin(radius * Mathf.PI / 2f), 0.8f);
+            radius *= r;
+            // 2 angles are generated from which a direction will be computed
+            float alpha = Random.Range(0f, Mathf.PI);
+            float theta = Random.Range(0f, Mathf.PI * 2f);
+
+            Vector3 pt = new Vector3(
+                radius * Mathf.Cos(theta) * Mathf.Sin(alpha),
+                radius * Mathf.Sin(theta) * Mathf.Sin(alpha),
+                radius * Mathf.Cos(alpha)
+            );
+
+            if (pt.y < -r/4)
+            {
+                continue;
+            }
+
+            // translation to match the parent position
+            pt += center;
+
+            points.Add(pt);
+        }
+        return points;
+    }
+
     public List<Vector3> GenerateAttractorsCube(int n, float r, Vector3 start)
     {
         List<Vector3> points = new List<Vector3>();
